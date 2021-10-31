c8d689c6c2f375abfc7ece0689b392ea19305edb
Kan <kan1657@hotmail.com>
2021-10-31 21:22:05 +0700

    Finish Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
-
+import Login from "./components/Login";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
 function App() {
-  return <div className="App">Let's build</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="/home">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
