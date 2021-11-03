aa3af9cb10af4292f2326369f8d31aef4db255e3
Kan <kan1657@hotmail.com>
2021-11-03 17:07:29 +0700

    PostModal: enable click start a post and show PostModal

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import PostModal from "./PostModal";
 
 const Main = () => {
+  const [showModal, setShowModal] = useState("close");
+  const handleClick = (e) => {
+    // console.log("showModal", showModal);
+    e.preventDefault();
+    if (e.target !== e.currentTarget) {
+      return;
+    }
+    switch (showModal) {
+      case "open":
+        setShowModal("close");
+        break;
+      case "close":
+        setShowModal("open");
+        break;
+      default:
+        setShowModal("close");
+        break;
+    }
+  };
   return (
     <Container>
       <ShareBox>
         Share
         <div>
           <img src="/images/user.svg" alt="" />
-          <button>Start a post</button>
+          <button onClick={handleClick}>Start a post</button>
         </div>
         <div>
           <button>
@@ -92,7 +111,7 @@ const Main = () => {
           </SocialActions>
         </Article>
       </div>
-      <PostModal />
+      <PostModal showModal={showModal} handleClick={handleClick} />
     </Container>
   );
 };
@@ -127,6 +146,7 @@ const ShareBox = styled(CommonCard)`
       display: flex;
       align-items: center;
       font-weight: 600;
+      cursor: pointer;
     }
     &:first-child {
       display: flex;
